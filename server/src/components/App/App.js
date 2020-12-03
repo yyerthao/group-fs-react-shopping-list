43af71937c93125641c9109c9fdb44b601ea6506
Cassen999 <69491352+Cassen999@users.noreply.github.com>
2020-12-03 10:34:11 -0600

    Initial commit

---
+++ server/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Songs!</h1>
+        </header>
+        <br/>
+        <p>Songs go here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="banner-header">
+          <h1>My Shopping List</h1>
+        </header>
+        <main>
+          <p>Under Construction...</p>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
