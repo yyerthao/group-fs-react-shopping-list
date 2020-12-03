01d3bebfb052582ce357a249c718623e0be26e00
Cassen Gerber <cassenpt@gmail.com>
2020-12-03 13:39:30 -0600

    added GET route on server side

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM shopping_list ORDER BY name ASC;`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
